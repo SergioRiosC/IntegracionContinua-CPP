84d2d9ff2b7b5cd5a315920eb6253d2325ef5c10
Sergio Ríos <sergiorc2000@hotmail.es>
2020-05-21 09:38:00 -0600

    Cambio en el test

--- test/Test.cpp
+++ test/Test.cpp
@@ -2,10 +2,10 @@
 #include "../src/Funciones.h"
 
 TEST(S,PS){
-    EXPECT_EQ(200,sum(5,15));
-    EXPECT_EQ(50,sum(1,4));
+    EXPECT_EQ(20,sum(5,15));
+    EXPECT_EQ(5,sum(1,4));
 }
 TEST(Resta, PruebaRes){
-    EXPECT_EQ(20,res(5,3));
-    EXPECT_EQ(50,res(8,3));
+    EXPECT_EQ(2,res(5,3));
+    EXPECT_EQ(5,res(8,3));
 }
