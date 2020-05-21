f5f88197089678e5c5e982afefcd4a34dabde137
Sergio Ríos <sergiorc2000@hotmail.es>
2020-05-21 09:02:45 -0600

    Cambios

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sergio/PrubaCPP
-# Build directory: /home/sergio/PrubaCPP/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(UnitTests "runUnitTests")
-subdirs("ext/googletest")
